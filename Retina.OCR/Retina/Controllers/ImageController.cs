896ede1f352b1e7fc880b9cefd0bee2c054d70e9
roo <roo@rhmg.co.uk>
2011-11-30 13:07:00 +0000

    support for sending to OCR

--- Retina.OCR/Retina/Controllers/ImageController.cs
+++ Retina.OCR/Retina/Controllers/ImageController.cs
@@ -2,8 +2,12 @@ using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
 using System.Web.Hosting;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace Retina.Controllers
 {
@@ -30,8 +34,52 @@ namespace Retina.Controllers
             }
             // save to temp dir with key
             target.Save(AppDomain.CurrentDomain.BaseDirectory + "/Temp/ocrImage.jpg", ImageFormat.Jpeg);
-            // communicate with the ocr
 
+            // communicate with the ocr
+            var imageUrl = "http://www.rhmg-files.co.uk/image1.png";
+            var key = "1hJy2bFmeFaoVpYk-1QD_Gsoe4cIDXYS";
+            var xe = new XElement("Job",
+                    new XElement("InputURL", imageUrl)
+                );
+            var wc = new WebClient();
+            wc.Headers[HttpRequestHeader.ContentType] = "text/xml";
+            ServicePointManager.Expect100Continue = false;
+            var results = wc.UploadString(
+                    "https://svc.webservius.com/v1/wisetrend/wiseocr/submit?wsvKey=" + key,
+                    "POST",
+                    xe.ToString()
+                );
+            string textURL = null;
+            //Use "dumb polling" every 2 seconds in this simplified example, until done.
+            //In a real application, notification with NotifyURL should be used instead.
+            while (true)
+            {
+                var resultsXml = XElement.Parse(results);
+                var jobURL = resultsXml.Element("JobURL").Value;
+                var status = resultsXml.Element("Status").Value;
+                if (status == "Finished")
+                {
+                    textURL = (
+                        from elem in resultsXml.Element("Download").Elements("File")
+                        where elem.Element("OutputType").Value == "TXT"
+                        select elem.Element("Uri").Value
+                       ).First();
+                    break;
+                }
+                //Exit if there's an error
+                if ((status != "Submitted") && (status != "Processing")) break;
+                Thread.Sleep(2000); // 2 seconds
+                results = wc.DownloadString(jobURL);
+            }
+            if (textURL == null)
+            {
+                Console.WriteLine("An error has occurred");
+            }
+            else
+            {
+                var text = wc.DownloadString(textURL);
+                Console.WriteLine(text);
+            }
             // get the terms back
 
             // return
