d18e2243771223b0c91da3c100a8c2fc90e94fa4
leee <leee@hotmail.co.uk>
2011-11-30 14:13:32 +0000

    post wip

--- Retina.App/assets/js/nicem.js
+++ Retina.App/assets/js/nicem.js
@@ -24,9 +24,51 @@ var nicem = (function () {
     }
 	
     function ajaxPageLoad (id) {
-		// This function setup to fire on each ajax page load.
-		console.log(id);
+		if (id === "image1") {
+			initCrop(id);
+			attachNextHijack(id);
+		}
     }
+	
+	function attachNextHijack (id) {
+		$('a[data-next-button="true"]').bind("click", function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			$("." + id).hide();
+			$.mobile.showPageLoadingMsg();
+			var data;
+			data.imageData = 
+			data.x = $('#x').val();
+			data.y = $('#y').val();
+			data.w = $('#w').val();
+			data.h = $('#h').val();
+			$.ajax({
+				type: "POST",
+				url: "http://www.ukfy.co.uk/image",
+				data: ,
+				success: function (data) {
+					$.mobile.hidePageLoadingMsg();
+					alert(data.result);
+				}
+				dataType: dataType
+			});
+			return false;
+		});
+	}
+	
+	function initCrop (id) {
+		$("." + id).Jcrop({
+			onChange: setCoordVals,
+			onSelect: setCoordVals
+		});
+	}
+	
+	function setCoordVals(c) {
+		$('#x').val(c.x);
+		$('#y').val(c.y);
+		$('#w').val(c.w);
+		$('#h').val(c.h);
+	};
 	
     return nicem;
 })();
