5344a09b203c57f00fcfb6c4aff5deb861128343
roo <roo@rhmg.co.uk>
2011-11-30 10:45:54 +0000

    c# code starts

---
+++ Retina.OCR/Retina/Global.asax.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Retina
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+            routes.MapRoute(
+                "Retina", 
+                "image", 
+                new { controller = "Image", action = "ocrImage" },
+                new { httpMethod = new HttpMethodConstraint("POST") });
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
